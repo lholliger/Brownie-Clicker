aa0405b678d9a1883df2470a467d8e59a9fb0d0d
DatOneLefty <k9dogs101@gmail.com>
2017-03-14 09:21:08 -0400

    block basic cheat

--- post.php
+++ post.php
@@ -19,11 +19,31 @@ if ($_GET['ACT'] == "GET_UUID") {
 		file_put_contents("$dir/username", "player");
 		file_put_contents("$dir/perclick", "1");
 		file_put_contents("$dir/persecond", "0");
-		file_put_contents("$dir/purch", "0;0;0;0;0;0;0;0;");
+		file_put_contents("$dir/total");
+		file_put_contents("$dir/purchace", "0;0;0;0;0;0;0;0;");
 		setcookie("BR_UUID", $UUID, time() + (10 * 365 * 24 * 60 * 60));
 		echo $UUID;
 	}
 
 
+}
+
+if ($_GET['ACT'] == "GET_BROWNIE") {
+	$UUID = microtime_float();
+	echo file_get_contents("../data/$UUID/total");
+}
+
+if ($_GET['ACT'] == "POST_TOTAL") {
+	$UUID = $_COOKIE['BR_UUID'];
+	$dir = "../data/$UUID"; 
+	$TOTAL = $_GET['TOTAL'];
+	$pc = file_get_contents("$dir/perclick");
+	$ps = file_get_contents("$dir/persecond");
+	$cu = file_get_contents("$dir/total");
+	if ($TOTAL > ($cu + ($pc * 15 * 5) + ($ps * 5))) {
+		echo "DENY-TOO-FAST";
+	} else {
+		echo "PASS-VER-TEST";
+	}
 }
 ?>
